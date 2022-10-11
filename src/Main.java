5cd7d76b77b2d69f0cbe90c955d279345e7c4181
Adnan <adnan.avni@hotmail.com>
2022-10-11 15:19:26 +0300

    omagui mainiin

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 
 public class Main { // Simulaattorin käynnistyspääohjelma
-	
+
 	public static void main(String args[]) {
-		
-		view.SimulaattorinGUI.main(args);
-		
+
+		view.OmaGUI.main(args);
+
 	}
 
 }
