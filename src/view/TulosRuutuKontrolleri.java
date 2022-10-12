6dcf77f0956fbfb9f4778acbd1baf1848ff3f02e
roopeky <roope.kylli@metropolia.fi>
2022-10-12 13:54:14 +0300

    tyhjenna tietokanta napin aloitus

--- src/view/TulosRuutuKontrolleri.java
+++ src/view/TulosRuutuKontrolleri.java
@@ -1,10 +1,22 @@
 package view;
 
+import java.io.IOException;
+
+import dao.IDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 public class TulosRuutuKontrolleri {
 	
@@ -83,10 +95,17 @@ public class TulosRuutuKontrolleri {
 	@FXML
 	private Label rinne2PalveluajanKA;
 	
+	@FXML
+	private Button tyhjennysNappi;
+	
 
 	public void initialize() {
 		ObservableList<String> availableChoices = FXCollections.observableArrayList("Kassa", "Vuokraamo","Kahvila","Rinne 1","Rinne2"); 
 		ppChoiceBox.setItems(availableChoices);
 	}
 
+	@FXML
+	public void tyhjennaTietokanta(ActionEvent event) {
+		
+	}
 }
